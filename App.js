23409023668b36f6826c53ebda6472829497f61f
bambwc20 <bambwc20@naver.com>
2021-04-23 03:43:47 +0900

    3.탭, 스택 네비게이션 조합

--- App.js
+++ App.js
@@ -3,13 +3,52 @@ import { NavigationContainer } from "@react-navigation/native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./components/HomeScreen";
-import SettingsScreen from "./components/SettingScreen";
-
-function log(data) {
-  console.log(data);
-}
+import DetailsScreen1 from "./components/DetailScreen1";
+import { createStackNavigator } from "@react-navigation/stack";
+import { View, Text } from "react-native";
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function HomeStackScreen() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#f4511e",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: "명지대",
+        }}
+      />
+      <Stack.Screen
+        name="Details"
+        component={DetailsScreen1}
+        options={{
+          title: "자유게시판",
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!</Text>
+      <Text>This page only watch!</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
@@ -40,7 +79,7 @@ export default function App() {
           inactiveTintColor: "gray", // 탭 비활성
         }}
       >
-        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Home" component={HomeStackScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
       </Tab.Navigator>
     </NavigationContainer>
--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, Button } from "react-native";
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Home!</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
     </View>
   );
 }
