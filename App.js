c88222d6c4024e4be5455be6bf55ae13e7a0d387
bambwc20 <bambwc20@naver.com>
2021-04-23 01:47:36 +0900

    1.스택스크린

--- App.js
+++ App.js
@@ -1,21 +1,103 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import "react-native-gesture-handler";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class DetailsScreen extends Component {
+  render() {
+    // console.log(this.props);
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => {
+            this.props.navigation.push("Details", {
+              name: "자유게시판 반복",
+            });
+          }}
+        />
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate("Home")}
+        />
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+        <Button
+          title="Go back to first screen in stack"
+          onPress={() => this.props.navigation.popToTop()}
+        />
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+class HomeScreen extends Component {
+  render() {
+    // console.log(this.props);
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="자유게시판"
+          onPress={() => {
+            this.props.navigation.push("Details", {
+              name: "자유게시판",
+            });
+          }}
+        />
+        <Button
+          title="Home 업데이트"
+          onPress={() =>
+            this.props.navigation.setOptions({ title: "Updated!" })
+          }
+        />
+        <Button
+          title="Home 되돌리기"
+          onPress={() => this.props.navigation.setOptions({ title: "명지대" })}
+        />
+      </View>
+    );
+  }
+}
+
+const Stack = createStackNavigator();
+
+class App extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: "#f4511e",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        >
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              title: "명지대",
+            }}
+          />
+          <Stack.Screen
+            name="Details"
+            component={DetailsScreen}
+            options={({ route }) => ({ title: route.params.name })}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
+
+export default App;
