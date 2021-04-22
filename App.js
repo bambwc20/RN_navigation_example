69e49b0a2cdc3bd82fdb1ca41781d135ed788562
bambwc20 <bambwc20@naver.com>
2021-04-23 01:48:21 +0900

    2.탭스크린 초기화면

--- App.js
+++ App.js
@@ -1,103 +1,48 @@
-import { StatusBar } from "expo-status-bar";
-import React, { Component } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
-import "react-native-gesture-handler";
-import { createStackNavigator } from "@react-navigation/stack";
+import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./components/HomeScreen";
+import SettingsScreen from "./components/SettingScreen";
 
-class DetailsScreen extends Component {
-  render() {
-    // console.log(this.props);
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Details Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => {
-            this.props.navigation.push("Details", {
-              name: "자유게시판 반복",
-            });
-          }}
-        />
-        <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate("Home")}
-        />
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
-        />
-        <Button
-          title="Go back to first screen in stack"
-          onPress={() => this.props.navigation.popToTop()}
-        />
-      </View>
-    );
-  }
+function log(data) {
+  console.log(data);
 }
 
-class HomeScreen extends Component {
-  render() {
-    // console.log(this.props);
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="자유게시판"
-          onPress={() => {
-            this.props.navigation.push("Details", {
-              name: "자유게시판",
-            });
-          }}
-        />
-        <Button
-          title="Home 업데이트"
-          onPress={() =>
-            this.props.navigation.setOptions({ title: "Updated!" })
-          }
-        />
-        <Button
-          title="Home 되돌리기"
-          onPress={() => this.props.navigation.setOptions({ title: "명지대" })}
-        />
-      </View>
-    );
-  }
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = "ios-home";
+            } else if (route.name === "Settings") {
+              iconName = "ios-settings";
+            }
+
+            if (focused === true) {
+              size = 25;
+            } else if (focused === false) {
+              size = 18;
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "tomato", // 탭 활성
+          inactiveTintColor: "gray", // 탭 비활성
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
-
-const Stack = createStackNavigator();
-
-class App extends Component {
-  render() {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: "#f4511e",
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-          }}
-        >
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{
-              title: "명지대",
-            }}
-          />
-          <Stack.Screen
-            name="Details"
-            component={DetailsScreen}
-            options={({ route }) => ({ title: route.params.name })}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
-  }
-}
-
-export default App;
